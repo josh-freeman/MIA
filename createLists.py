7e7f84f25b58663b9bba3b34d21a50ab62d39136
charliebobjosh <47647715+charliebobjosh@users.noreply.github.com>
2020-06-14 19:02:09 +0200

    Add files via upload

--- createLists.py
+++ createLists.py
@@ -15,6 +15,10 @@ def printHtml(url):
 
     return mystr
 
+def getToken(testBot:bool):
+   str_data = open('tokens.json').read()
+   token_data = json.loads(str_data)
+   return token_data["testBot" if testBot else "realBot"]
 
 def startupCheck(filename,s):
     if os.path.exists(filename) and os.access('.', os.R_OK):
