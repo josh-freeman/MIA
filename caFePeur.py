22f6e61f5027d8481b1fc9d36c4fe56088561076
charliebobjosh <47647715+charliebobjosh@users.noreply.github.com>
2020-06-28 19:07:15 +0200

    Delete caFePeur.py

--- caFePeur.py
+++
@@ -1,8 +0,0 @@
-import urllib.request, json
-from random import choice
-
-
-def randomGifUrl(search):
-    data = json.loads(urllib.request.urlopen(
-        "http://api.giphy.com/v1/gifs/search?q="+search+"&api_key=n50bkKdN6ZQdr6wsKjvzHDBcTN1Sx9Is&limit=20").read())
-    return choice(data["data"])["images"]["downsized_large"]["url"]
