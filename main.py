9888e94af54ac569a809c786972d81d1c02c991b
charliebobjosh <47647715+charliebobjosh@users.noreply.github.com>
2020-06-28 19:06:23 +0200

    Add files via upload (#25)
    (j'ai pas testé avec les updates que tas fais, donc je sais pas si ça va run comme tu le veux)
    
    Co-authored-by: TheFuser <40115135+TheFuser@users.noreply.github.com>

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import json
 from createLists import startupCheck, appendFrom, getToken
 from game import jeu, MauvaisIndice
 import discord
-from caFePeur import randomGifUrl
+from commandes import weather, randomGifUrl
 
-token = getToken("joshibot")  # "joshibot" or "testbot"
+token = getToken("joshibot") #either "joshibot" or "testbot"
 client = discord.Client()
 lock = asyncio.Lock()
-
+default_prefix="<"
 
 async def partieProMessage(j: jeu, message):
     embed = discord.Embed(title="Partie pro lancée", color=0x00ff00)
@@ -29,14 +29,14 @@ async def on_ready():
         appendFrom("https://fr.m.wiktionary.org/wiki/Utilisateur:Darkdadaah/Listes/Mots_dump/frwiki/2016-02-03",
                    "FR", json_data)
         appendFrom("http://corpus.rae.es/frec/10000_formas.TXT", "ES", json_data)
-
         print("FR : ", len(json_data["FR"]), "EN : ", len(json_data["EN"]), "ES : ", len(json_data["ES"]))
+
 
     startupCheck('guilds.json', json.dumps([{}]))
     str_data = open('guilds.json').read()
     json_guilds = json.loads(str_data)
     await client.wait_until_ready()
-    await client.change_presence(activity=discord.Activity(name=f"{len(client.guilds)} servs. Bien réveillé.",
+    await client.change_presence(activity=discord.Activity(name=f"{len(client.guilds)} servs. def Prefix: {default_prefix}",
                                                            type=discord.ActivityType.watching))
     json_guilds[0]["joueurs"] = {}
     for guild in client.guilds:
@@ -76,7 +76,7 @@ async def on_member_remove(member):
 
 @client.event
 async def on_guild_join(guild):
-    await client.change_presence(activity=discord.Activity(name=f"{len(client.guilds)} servs. Bien réveillé.",
+    await client.change_presence(activity=discord.Activity(name=f"{len(client.guilds)} servs. Default Prefix: {default_prefix}",
                                                            type=discord.ActivityType.watching))
     str_data = open('guilds.json').read()
     json_guilds = json.loads(str_data)
@@ -93,17 +93,16 @@ async def on_guild_join(guild):
                                         channel.id: jeu(1, "FR", 1, 0, {}, False, False, False, "").getAttributes() for
                                         channel in guild.text_channels},
                                     }
-        for member in guild.members:
-            if not member.id in json_guilds[0]["joueurs"]:
-                json_guilds[0]["joueurs"][member.id] = {"XP": 0}
     json.dump(json_guilds, open('guilds.json', 'w'), indent=2)
-    await guild.channels[i].send("Coucou '{}'!".format(guild.name))
+    await guild.channels[i].send("Coucou '{}'<".format(guild.name))
+    await guild.channels[i].send("Bien réveillé")
 
 
 @client.event
 async def on_guild_remove(guild):
-    await client.change_presence(activity=discord.Activity(name=f"{len(client.guilds)} servs. Bien réveillé.",
-                                                           type=discord.ActivityType.watching))
+    await client.change_presence(
+        activity=discord.Activity(name=f"{len(client.guilds)} servs. Prefix : {default_prefix}",
+                                  type=discord.ActivityType.watching))
     str_data = open('guilds.json').read()
     json_guilds = json.loads(str_data)
     json_guilds[0].pop(str(guild.id), None)
@@ -128,12 +127,20 @@ async def on_guild_channel_create(channel):
         ]
     json.dump(json_guilds, open('guilds.json', 'w'), indent=2)
 
+
 @client.event
 async def on_guild_channel_delete(channel):
     str_data = open('guilds.json').read()
     json_guilds = json.loads(str_data)
     json_guilds[0][str(channel.guild.id)]["SalonsEtJeuxEnCoursAssocies"].pop(str(channel.id), None)
     json.dump(json_guilds, open('guilds.json', 'w'), indent=2)
+
+
+
+
+
+
+
 
 @client.event
 async def on_message(message):
@@ -154,6 +161,7 @@ async def on_message(message):
                 attributs[6], attributs[7], attributs[8])
         if message.content.startswith(prefix):
             text = message.content[1:]
+
             if text.startswith("ca fé peur"):
                 if message.author.id == 499302416106258432 or message.author.id == 193424451013050368:
                     randomHorrorGif = discord.Embed(color=0xee82ee)
@@ -164,7 +172,7 @@ async def on_message(message):
 
             elif text.startswith("pikachu"):
                 if message.author.id == 499302416106258432 or message.author.id == 330438546710331413:
-                    randomPikaGif = discord.Embed(title="Un pikachu sauvage est apparu !",color=0xee82ee)
+                    randomPikaGif = discord.Embed(title="Un pikachu sauvage est apparu !", color=0xee82ee)
                     randomPikaGif.set_image(url=randomGifUrl("pikachu"))
                     await message.channel.send(embed=randomPikaGif)
                 else:
@@ -178,18 +186,30 @@ async def on_message(message):
                         await message.channel.send(len(deleted))
                     except discord.errors.Forbidden:
                         await message.channel.send("`403 Forbidden` : Je n'ai pas la permission (errorCode 50013)")
+
                 else:
                     await message.channel.send("T'es pas joshinou")
+
             if text.startswith("prefixe "):
                 text = text[len("prefixe "):]
                 if len(text) == 1:
                     if message.author.id == 499302416106258432:
                         json_guilds[0][str(message.guild.id)]["prefix"] = text
                         await message.channel.send("prefixe : '%s'" % json_guilds[0][str(message.guild.id)]["prefix"])
+                        print(text)
+                        await client.change_presence(
+                            activity=discord.Activity(name=f"{len(client.guilds)} servs. Prefix : \"{text}\"",
+                                                      type=discord.ActivityType.watching))
+                        default_prefix=text
                     else:
                         await message.channel.send("T'es pas joshinou (pas de permission)")
                 else:
                     await message.channel.send("%s ? C'est pas un préfixe, ça, gros beta..." % text)
+
+            if text.startswith("weather"): #Commande secrète, n'est pas sur le Bot officiel pour le moment
+                if message.author.id == 358629457025826816:
+                    await message.channel.send(embed=weather(text))
+
             if text.startswith('help'):
                 helpMessage = discord.Embed(color=0x00ff00)
                 helpMessage.set_author(name="Joshinou", url="https://github.com/charliebobjosh")
@@ -199,10 +219,9 @@ async def on_message(message):
                                       inline=False)
                 helpMessage.add_field(name="Anagame - Jeu d'anagrammes", value=
                 ":book: `" + prefix + "anagramme(s) ` pour partie simple (1 tour, niveau 1 en FR).\n"
-
                                       ":book: `" + prefix + "anagramme(s) [niveau]` pour préciser le niveau (1 tour en FR).\n"
-                                                            ":book: `" + prefix + "anagramme(s) [EN/FR]` pour préciser la langue (1 tour, niveau 1).\n"
-                                                                                  ":book: `" + prefix + "anagramme(s) [niveau] EN/FR [nombre de tours]` pour lancer une partie pro."
+                                                            ":book: `" + prefix + "anagramme(s) [EN/FR/ES]` pour préciser la langue (1 tour, niveau 1).\n"
+                                                                                  ":book: `" + prefix + "anagramme(s) [niveau] EN/FR/ES [nombre de tours]` pour lancer une partie pro."
                                                                                                         "\nEn cours de partie,`" + prefix + "anagramme(s) ` arrête la partie."
                                                                                                                                             "\nLe niveau max est `%i` en anglais, `%i` en français, `%i` en espagnol (niveau min 1)." % (
                     jeu(1, "EN", 1, 0, {}, False, False, False, "").niveauMax,
@@ -227,7 +246,6 @@ async def on_message(message):
 
                 elif j.tourNumero < j.nbTours + 1:
                     essai = j.decode(text)
-
                     if essai == j.decode(j.mot):
 
                         await message.add_reaction("😄")
@@ -270,6 +288,7 @@ async def on_message(message):
 
 
             else:
+
                 if text.startswith('anagramme'):
                     erreur = False
                     try:
@@ -284,17 +303,24 @@ async def on_message(message):
                         try:
                             j = jeu(1, textList[0].upper(), 1, 0, {}, False, False, False,
                                     "")  # Alors le bot lance une partie avec Niveau 1, en FR, à 1 tour
-
                         except MauvaisIndice:
                             j = jeu(int(textList[0]), "FR", 1, 0, {}, False, False, False, "")
                         except:
                             await message.channel.send(
                                 "Mauvaise saisie : ` !anagramme(s) [niveau ou EN/FR]`")
                             erreur = True
-
                     elif not erreur and len(textList) == 3:
                         try:
                             j = jeu(int(textList[0]), textList[1].upper(), int(textList[2]), 0, {}, False, False, False,
+                                    "")
+                            await partieProMessage(j, message)
+                        except MauvaisIndice as inst:
+                            await message.channel.send(inst)
+                            erreur = True
+
+                    elif not erreur and len(textList) == 3:
+                        try:
+                            j = jeu(int(textList[0]), textList[1] == 'FR', int(textList[2]), 0, {}, False, False, False,
                                     "")
                             await partieProMessage(j, message)
                         except MauvaisIndice as inst:
