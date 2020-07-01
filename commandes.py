01b904a27679a20fa5e4e153fb9300019543f659
charliebobjosh <47647715+charliebobjosh@users.noreply.github.com>
2020-07-01 10:45:40 +0200

    bugs introduits par la dernière upload

--- commandes.py
+++ commandes.py
@@ -7,9 +7,11 @@ import discord
 import urllib.request, json
 from random import choice
 
-data=json.loads(urllib.request.urlopen("http://api.giphy.com/v1/gifs/search?q=horror&api_key=n50bkKdN6ZQdr6wsKjvzHDBcTN1Sx9Is&limit=20").read())
 
-def randomGifUrl():
+def randomGifUrl(search):
+    data = json.loads(urllib.request.urlopen(
+        "http://api.giphy.com/v1/gifs/search?q="+search+"&api_key=n50bkKdN6ZQdr6wsKjvzHDBcTN1Sx9Is&limit=20").read())
+
     return choice(data["data"])["images"]["downsized_large"]["url"]
 
 
